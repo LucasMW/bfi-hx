0564ced09d639cd96d2db063f1153b07cf9721d5
LucasMW <great-lucas@hotmail.com>
2018-09-18 11:09:39 -0300

    handle arguments

--- src/Main.hx
+++ src/Main.hx
@@ -1,9 +1,26 @@
 package;
 class Main {
     public static function main(){
-        var bfi = new Brainfuck("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.");
+
+        var args : Array<String> = Sys.args();
+        if(args.length < 1) {
+            Sys.println("Usage is ./" + Sys.programPath() + " filename");
+            Sys.exit(1);
+        }
+        var path = args[0];
+        var program = sys.io.File.getContent(path);
+        var bfi = new Brainfuck(program);
         bfi.run();
     }
-    
+    static function readLine(fileName, lineNumber) : String {
+    var fin = sys.io.File.read(fileName, false);
+    try {
+        for (i in 0...lineNumber)
+            fin.readLine();
+        var line = fin.readLine();
+        fin.close();
+        return line;
+    } catch (e:haxe.io.Eof) { return null; } 
+}
 }
     
